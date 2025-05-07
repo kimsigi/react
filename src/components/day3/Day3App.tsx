383975f643d6df8d81fca0df7603afeefc45ba2f
kps <dev.kps88@gmail.com>
2025-05-07 15:07:00 +0900

    commit

--- src/components/day3/Day3App.tsx
+++ src/components/day3/Day3App.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import ClickEvent from "./ClickEvent";
 import FruitList from "./FruitList";
 import Greeting from "./Greeting";
 
 export default function Day3App() {
+    const [isLogin, setIsLogin] = useState(false);
+
     return (
         <>
             <h1>🗓️ React 학습 3일차: 조건부 렌더링 + 리스트 렌더링 + 이벤트 핸들링</h1>
-            <Greeting isLoggedIn={false} />
+            <Greeting isLoggedIn={isLogin} />
+            <button onClick={() => setIsLogin(!isLogin)}>로그인토글</button>
             <br />
             <FruitList />
             <br />
---
+++ src/components/day4/Day4App.tsx
@@ -0,0 +1,8 @@
+export default function Day4App() {
+    return (
+        <>
+            <h1>🗓️ React 학습 4일차: useEffect로 생명주기 다루기</h1>
+            
+        </>
+    );
+}
